117dbb230c7b80dd8136189773c8546887d789d7
vincentmin <vincentmin17@gmail.com>
2023-08-20 21:17:12 +0200

    refactor syntax

--- src/main/scala/base/runnable.scala
+++ src/main/scala/base/runnable.scala
@@ -4,12 +4,10 @@ trait Runnable {
   def run(input: StringInput): Output
 
   private def toStringInput(input: Input): StringInput =
-    val stringInputs = input.collect(_ match
-      case (key, value: String) => (key, value)
-    )
-    val evaluatedRunnables = input.values.collect(_ match
-      case runnable: Runnable => runnable.run(stringInputs)
-    )
+    val stringInputs = input.collect { case (k, v: String) => (k, v) }
+    val evaluatedRunnables = input.values.collect { case runnable: Runnable =>
+      runnable.run(stringInputs)
+    }
     evaluatedRunnables.foldLeft(stringInputs) { (acc, output) =>
       if acc.keys.exists(output.contains(_)) then
         println(s"Warning: key already exists in input: $output")
